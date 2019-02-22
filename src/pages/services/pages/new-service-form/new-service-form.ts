62e0b1d779fa76ab56834dcf6ec06fc8a8e4fc89
hnamzian <namazian87@gmail.com>
2019-02-22 11:33:57 +0330

    service validators added

--- src/pages/services/pages/new-service-form/new-service-form.ts
+++ src/pages/services/pages/new-service-form/new-service-form.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
-import { NavController, ViewController, PopoverController } from "ionic-angular";
+import {
+  NavController,
+  ViewController,
+  PopoverController,
+  ToastController,
+  Toast
+} from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { NewServiceSheetPage } from "../new-service-sheet/new-service-sheet";
-import { ServiceItem, Service } from "../../../../models/service";
+import { Service } from "../../../../models/service";
 import { Car } from "../../../../models/car";
 import { CarStorage } from "../../../../storage/car/car";
 
@@ -12,41 +19,77 @@ import { CarStorage } from "../../../../storage/car/car";
 export class NewServiceFormPage {
   selectedCar = {} as Car;
 
-  title;
-  garage;
-  value;
-  date;
-  comment;
+  serviceForm: FormGroup;
 
-  serviceList = {} as Service;
+  toast: Toast;
 
   constructor(
     public navCtrl: NavController,
     public viewCtrl: ViewController,
+    public formBuilder: FormBuilder,
+    public toastCtrl: ToastController,
     public popoverCtrl: PopoverController,
     public carStorage: CarStorage
   ) {}
 
   async ngOnInit() {
+    this.serviceForm = this.formBuilder.group({
+      date: ["", Validators.required],
+      title: [""],
+      garage: [""],
+      totalCost: ["", Validators.required],
+      comment: [""]
+    });
+
     this.selectedCar = await this.carStorage.getSelectedCar();
     console.log(this.selectedCar);
   }
 
   addServiceForm() {
-    this.serviceList.title = this.title;
-    this.serviceList.carId = this.selectedCar.id;
-    this.serviceList.date = this.date;
-    this.serviceList.totalCost = this.value;
-    this.serviceList.garageName = this.garage;
+    if (this.serviceForm.invalid) {
+      const errorMessage = this.formErrorCheck();
+      return this.showToast(errorMessage);
+    }
+
+    const serviceList = {
+      title: this.serviceForm.get("title").value,
+      carId: this.selectedCar.id,
+      date: this.serviceForm.get("date").value,
+      totalCost: this.serviceForm.get("totalCost").value,
+      garageName: this.serviceForm.get("garage").value
+    } as Service;
 
     let popover = this.popoverCtrl.create(
       NewServiceSheetPage,
-      { serviceList: this.serviceList },
+      { serviceList: serviceList },
       { cssClass: "servicePopover" }
     );
     popover.present();
     popover.onDidDismiss(data => {
       this.viewCtrl.dismiss({}, "", { animate: false });
     });
+  }
+
+  formErrorCheck() {
+    const message = this.serviceForm.get("date").hasError("required")
+      ? "تاریخ الزامی است"
+      : this.serviceForm.get("totalCost").hasError("required")
+      ? "هزینه نامعتبر است"
+      : "خطا";
+    return message;
+  }
+
+  showToast(message) {
+    this.toast = this.toastCtrl.create({
+      message: message,
+      position: "bottom",
+      duration: 2000,
+      cssClass: "toast"
+    });
+    this.toast.present();
+  }
+
+  dismissToast() {
+    this.toast.dismiss();
   }
 }
