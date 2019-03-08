dda1e6f1610988e825efa5c766bbad054ab612b5
hnamzian <namazian87@gmail.com>
2019-03-08 16:04:55 +0330

    refreshing services list added on init page

--- src/pages/services/pages/services-list/services-list.ts
+++ src/pages/services/pages/services-list/services-list.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { NavController, PopoverController, LoadingController, Loading } from "ionic-angular";
+import { NavController, PopoverController, LoadingController, Loading, ToastController, Toast } from "ionic-angular";
 import { ServiceDataPage } from "../service-data/service-data";
 import { NewServiceFormPage } from "../new-service-form/new-service-form";
 import { ServicesProvider } from "../../../../providers/services/services";
@@ -21,21 +21,20 @@ export class ServicesListPage implements OnInit {
   services = [] as Service[];
   newServiceItems;
 
+  toast: Toast;
   loading: Loading;
 
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public popoverCtrl: PopoverController, public carStorage: CarStorage, public servicesProvider: ServicesProvider) {}
+  constructor(
+    public navCtrl: NavController,
+    public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController,
+    public popoverCtrl: PopoverController,
+    public carStorage: CarStorage,
+    public servicesProvider: ServicesProvider
+  ) {}
 
   async ngOnInit() {
-    this.selectedCar = await this.carStorage.getSelectedCar();
-    let services$ = await this.servicesProvider.getServices(this.selectedCar.id);
-    services$.subscribe(result => {
-      this.services = result.periodicServices;
-      this.services = this.services.map(service => {
-        service.date = moment(service.date).format("YYYY-MM-DD");
-        return service;
-      });
-      console.log(this.services);
-    });
+    await this.refreshServicesList();
   }
 
   addService() {
@@ -67,6 +66,39 @@ export class ServicesListPage implements OnInit {
 
   navToHome() {
     this.navCtrl.push(VehicleMenuPage);
+  }
+
+  async refreshServicesList() {
+    this.showLoader();
+    await this.getServices();
+    this.dismissLoader();
+  }
+
+  async getServices() {
+    this.selectedCar = await this.carStorage.getSelectedCar();
+    let services$ = await this.servicesProvider.getServices(this.selectedCar.id);
+    services$.subscribe(result => {
+      this.services = result.periodicServices;
+      this.services = this.services.map(service => {
+        service.date = moment(service.date).format("YYYY-MM-DD");
+        return service;
+      });
+      console.log(this.services);
+    });
+  }
+
+  showToast(message) {
+    this.toast = this.toastCtrl.create({
+      message: message,
+      position: "bottom",
+      duration: 2000,
+      cssClass: "toast"
+    });
+    this.toast.present();
+  }
+
+  dismissToast() {
+    this.toast.dismiss();
   }
 
   showLoader() {
