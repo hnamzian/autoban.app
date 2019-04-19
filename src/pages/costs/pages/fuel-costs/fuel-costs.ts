d3be30eb88ab8cd3918345908a1fcef348bc18f3
hnamzian <namazian87@gmail.com>
2019-04-19 18:11:47 +0430

    loading css class added to fuel-cost page

--- src/pages/costs/pages/fuel-costs/fuel-costs.ts
+++ src/pages/costs/pages/fuel-costs/fuel-costs.ts
@@ -101,7 +101,8 @@ export class FuelCostsPage implements OnInit {
 
   showLoader() {
     this.loading = this.loadingCtrl.create({
-      showBackdrop: false
+      showBackdrop: false,
+      cssClass: 'loadingClass'
     });
 
     this.loading.present();
